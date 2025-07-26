4ee306980319cd1e9f258bf0f3520c6f634569f6
Basil Ermatinger <62306996+Vrijl@users.noreply.github.com>
2025-07-26 10:13:09 +0200

    Examples for arrays

---
+++ 02-basics-and-basics-types/arrays-objects.js
@@ -0,0 +1,5 @@
+var hobbies;
+hobbies = ["Sports", "Cooking"];
+console.log(hobbies);
+hobbies.push("Gardening");
+console.log(hobbies);
---
+++ 02-basics-and-basics-types/arrays-objects.ts
@@ -0,0 +1,6 @@
+let hobbies : string[];
+hobbies = ["Sports", "Cooking"];
+console.log(hobbies)
+
+hobbies.push("Gardening");
+console.log(hobbies);
