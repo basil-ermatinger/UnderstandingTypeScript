f626a35fcd28a5f242e015b87a0bffbf9fe03009
Basil Ermatinger <62306996+Vrijl@users.noreply.github.com>
2025-07-26 10:18:02 +0200

    Examples for Advanced Array Types

--- 02-basics-and-basics-types/arrays-objects.js
+++ 02-basics-and-basics-types/arrays-objects.js
@@ -1,5 +1,20 @@
+// Arrays
+console.log("---------------------");
+console.log("Arrays");
+console.log("---------------------");
 var hobbies;
 hobbies = ["Sports", "Cooking"];
 console.log(hobbies);
 hobbies.push("Gardening");
 console.log(hobbies);
+// Advanced Array Types
+console.log("---------------------");
+console.log("Advanced Array Types");
+console.log("---------------------");
+var users;
+users = ["Basil", 12];
+console.log(users);
+users = [12, "Basil"];
+console.log(users);
+users = ["Basil", "Ermatinger"];
+console.log(users);
--- 02-basics-and-basics-types/arrays-objects.ts
+++ 02-basics-and-basics-types/arrays-objects.ts
@@ -1,6 +1,27 @@
+// Arrays
+console.log("---------------------")
+console.log("Arrays");
+console.log("---------------------")
+
 let hobbies : string[];
 hobbies = ["Sports", "Cooking"];
 console.log(hobbies)
 
 hobbies.push("Gardening");
 console.log(hobbies);
+
+// Advanced Array Types
+console.log("---------------------")
+console.log("Advanced Array Types");
+console.log("---------------------")
+
+let users : (string | number)[];
+
+users = ["Basil", 12];
+console.log(users);
+
+users = [12, "Basil"];
+console.log(users);
+
+users = ["Basil", "Ermatinger"];
+console.log(users);
