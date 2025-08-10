c41a7cd0af9af7119513c2afc14a8ad13a2b83f7
Basil Ermatinger <62306996+Vrijl@users.noreply.github.com>
2025-08-10 10:42:46 +0200

    Example for package.json and adjusted .gitignore to ignore Node.js dependencies

--- 03-typescript-compiler/src/app.ts
+++ 03-typescript-compiler/src/app.ts
@@ -1,5 +1,13 @@
+import fs from "node:fs";
+
 let userName: string;
 
 userName = "test";
 
 console.log(userName);
+
+function add(a: number, b: number) {
+  return a + b;
+}
+
+console.log(add(1, 2));
