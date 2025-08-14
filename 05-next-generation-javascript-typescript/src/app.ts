8dc5e5d162cd9e13cb31195225f586e52241fda1
Basil Ermatinger <62306996+Vrijl@users.noreply.github.com>
2025-08-15 00:11:37 +0200

    added examples for ES6 features let/const, arrow functions and default function paramters.

--- 05-next-generation-javascript-typescript/src/app.ts
+++ 05-next-generation-javascript-typescript/src/app.ts
@@ -1,1 +1,38 @@
-// Code goes here!
+// Examples for const and let
+
+const userName = "Basil";
+let age = 30;
+
+age = 29;
+
+function add(a: number, b: number) {
+  let result;
+  result = a + b;
+  return result;
+}
+
+// Examples of arrow functions
+
+const addAsArrowFunc = (a: number, b: number) => {
+  return a + b;
+}
+
+const addAsArrowFuncShort = (a: number, b: number) => a + b;
+
+const printOutput: (a: number | string) => void = output => console.log(output);
+
+console.log(addAsArrowFunc(2, 5));
+console.log(addAsArrowFuncShort(10, 10));
+printOutput(add(8, 8));
+
+const button = document.querySelector("button");
+
+if(button) {
+  button.addEventListener("click", event => console.log(event));
+}
+
+// Examples for default function parameters
+
+const addWithDefault = (a: number, b: number = 2) => a + b;
+
+console.log(addWithDefault(3));
