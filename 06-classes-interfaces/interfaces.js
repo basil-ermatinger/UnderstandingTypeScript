0b6cac59c4ff578e8237049da86899e4d82c63b4
Basil Ermatinger <62306996+Vrijl@users.noreply.github.com>
2025-08-22 18:44:27 +0200

    Examples for mergin interfaces and using interfaces with classes.

--- 06-classes-interfaces/interfaces.js
+++ 06-classes-interfaces/interfaces.js
@@ -4,8 +4,9 @@ let user;
 user = {
     email: "test@example.com",
     password: "abc1",
+    role: "admin",
     login() {
-        console.log(`Login with email ${this.email} and password ${this.password}`);
+        console.log(`Login with email ${this.email} and password ${this.password} and role ${this.role}`);
     },
     logout() {
         console.log("Logout");
@@ -13,3 +14,25 @@ user = {
 };
 user.login();
 user.logout();
+// implementing interfaces
+class AuthenticatableUser {
+    userName;
+    email;
+    password;
+    role;
+    constructor(userName, email, password, role) {
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+    login() {
+        console.log(`${this.userName} is logging in with email ${this.email} and password ${this.password} and role ${this.role}`);
+    }
+    logout() {
+        console.log("Logout");
+    }
+}
+const authenticatableUser = new AuthenticatableUser("Basil", "basil@test.ch", "abc123", "admin");
+authenticatableUser.login();
+authenticatableUser.logout();
--- 06-classes-interfaces/interfaces.ts
+++ 06-classes-interfaces/interfaces.ts
@@ -5,6 +5,12 @@ interface IAuthenticatable {
   logout(): void;
 }
 
+// Merge interface
+
+interface IAuthenticatable {
+  role: string;
+}
+
 // interface as object type
 
 let user: IAuthenticatable;
@@ -12,8 +18,9 @@ let user: IAuthenticatable;
 user = {
   email: "test@example.com",
   password: "abc1",
+  role: "admin",
   login() {
-    console.log(`Login with email ${this.email} and password ${this.password}`);
+    console.log(`Login with email ${this.email} and password ${this.password} and role ${this.role}`);
   },
   logout() {
     console.log("Logout");
@@ -22,3 +29,22 @@ user = {
 
 user.login();
 user.logout();
+
+// implementing interfaces
+
+class AuthenticatableUser implements IAuthenticatable {
+  constructor(public userName: string, public email: string, public password: string, public role: string) {}
+
+  login() {
+    console.log(`${this.userName} is logging in with email ${this.email} and password ${this.password} and role ${this.role}`);
+  }
+
+  logout() {
+    console.log("Logout");
+  }
+}
+
+const authenticatableUser: AuthenticatableUser = new AuthenticatableUser("Basil", "basil@test.ch", "abc123", "admin");
+
+authenticatableUser.login();
+authenticatableUser.logout();
