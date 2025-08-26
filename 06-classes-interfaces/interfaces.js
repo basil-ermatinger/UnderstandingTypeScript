bd997551556f074cb865dbcc3809ffbf18a9619c
Basil Ermatinger <62306996+Vrijl@users.noreply.github.com>
2025-08-26 20:16:15 +0200

    Exmaples for ensuring base types with interfaces and extending interfaces.

--- 06-classes-interfaces/interfaces.js
+++ 06-classes-interfaces/interfaces.js
@@ -36,3 +36,36 @@ class AuthenticatableUser {
 const authenticatableUser = new AuthenticatableUser("Basil", "basil@test.ch", "abc123", "admin");
 authenticatableUser.login();
 authenticatableUser.logout();
+// Ensuring base types with interfaces
+const authenticate = (user) => {
+    user.email = "auth@test.ch";
+    user.password = "abc";
+    user.role = "user";
+    user.login();
+    user.logout();
+};
+authenticate(user);
+class AdminUser {
+    userName;
+    email;
+    password;
+    role;
+    adminPermission;
+    constructor(userName, email, password, role, adminPermission) {
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.adminPermission = adminPermission;
+    }
+    login() {
+        console.log(`${this.userName} is logging in with email ${this.email} and password ${this.password} and role ${this.role}`);
+        console.log(`Has admin permission \"${this.adminPermission}\"`);
+    }
+    logout() {
+        console.log("Admin logout");
+    }
+}
+const adminUser1 = new AdminUser("AdminName", "admin@test.ch", "abc123", "administrator", "superadmin");
+adminUser1.login();
+adminUser1.logout();
--- 06-classes-interfaces/interfaces.ts
+++ 06-classes-interfaces/interfaces.ts
@@ -48,3 +48,41 @@ const authenticatableUser: AuthenticatableUser = new AuthenticatableUser("Basil"
 
 authenticatableUser.login();
 authenticatableUser.logout();
+
+// Ensuring base types with interfaces
+
+const authenticate = (user: IAuthenticatable): void => {
+  user.email = "auth@test.ch";
+  user.password = "abc";
+  user.role = "user";
+
+  user.login();
+  user.logout();
+}
+
+authenticate(user);
+
+// Extending interfaces
+
+interface IAuthenticatableAdmin extends IAuthenticatable {
+  adminPermission: "admin" | "superadmin";
+}
+
+class AdminUser implements IAuthenticatableAdmin {
+  constructor(public userName: string, public email: string, public password: string, public role: string, public adminPermission: "admin" | "superadmin") {}
+
+  login() {
+    console.log(`${this.userName} is logging in with email ${this.email} and password ${this.password} and role ${this.role}`);
+    console.log(`Has admin permission \"${this.adminPermission}\"`);
+  }
+
+  logout() {
+    console.log("Admin logout");
+  }
+}
+
+const adminUser1 = new AdminUser("AdminName", "admin@test.ch", "abc123", "administrator", "superadmin");
+
+adminUser1.login();
+adminUser1.logout();
+
