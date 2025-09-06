2a7361c901f3a5b0ffb6fb1d07f56a27a29016e8
Basil Ermatinger <62306996+Vrijl@users.noreply.github.com>
2025-09-06 10:28:07 +0200

    Examples for constant types with const and the "satisfies" keyword

--- 07-advanced-types/advanced.js
+++ 07-advanced-types/advanced.js
@@ -9,3 +9,32 @@ if (store.isOpen) {
 else {
     console.log(`Store with Id ${store.id} is closed`);
 }
+// constant types with "as const"
+let roles = ["admin", "guest", "editor"];
+roles.push("visitor");
+const firstRole = roles[0];
+console.log(firstRole);
+let rolesConst = ["admin", "guest", "editor"];
+// rolesConst.push("visitor") -> won't work because rolesConst is readonly
+const firstRoleConst = rolesConst[0];
+console.log(firstRoleConst);
+// "satisfies" keyword
+const dataEntries = {
+    entry1: 0.51,
+    entry2: -1.23
+};
+dataEntries.entry3 = 42;
+console.log(dataEntries.entry1);
+console.log(dataEntries.entry3);
+console.log(dataEntries.entry4);
+const dataEntriesSatisfied = {
+    entry1: 1,
+    entry2: 2
+};
+// dataEntriesSatisfied.entry3 = 3; > won't work
+console.log(dataEntriesSatisfied.entry1);
+const dataEntriesWithContract = {
+    entry1: 5,
+    entry2: 6
+};
+console.log(dataEntriesWithContract.entry1);
--- 07-advanced-types/advanced.ts
+++ 07-advanced-types/advanced.ts
@@ -14,3 +14,51 @@ if(store.isOpen) {
 } else {
   console.log(`Store with Id ${store.id} is closed`);
 }
+
+// constant types with "as const"
+
+let roles = ["admin", "guest", "editor"];
+roles.push("visitor");
+const firstRole = roles[0];
+
+console.log(firstRole);
+
+let rolesConst = ["admin", "guest", "editor"] as const;
+// rolesConst.push("visitor") -> won't work because rolesConst is readonly
+
+const firstRoleConst = rolesConst[0];
+
+console.log(firstRoleConst);
+
+// "satisfies" keyword
+
+const dataEntries: Record<string, number> = {
+  entry1: 0.51,
+  entry2: -1.23
+}
+
+dataEntries.entry3 = 42;
+
+console.log(dataEntries.entry1);
+console.log(dataEntries.entry3);
+console.log(dataEntries.entry4);
+
+const dataEntriesSatisfied = {
+  entry1: 1,
+  entry2: 2
+} satisfies Record<string, number>;
+
+// dataEntriesSatisfied.entry3 = 3; > won't work
+
+console.log(dataEntriesSatisfied.entry1);
+// console.log(dataEntriesSatisfied.entry3); > won't work
+
+type satisfiedContract = Record<string, number>;
+
+const dataEntriesWithContract = {
+  entry1: 5,
+  entry2: 6
+} satisfies satisfiedContract;
+
+console.log(dataEntriesWithContract.entry1);
+
