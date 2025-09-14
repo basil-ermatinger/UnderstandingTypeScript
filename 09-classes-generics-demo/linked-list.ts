db16fd14873f48b9ea6efc4c3c6ef21695b3660b
Basil Ermatinger <62306996+Vrijl@users.noreply.github.com>
2025-09-14 12:12:57 +0200

    Set generic type for linked list and added logic for adding notes.

--- 09-classes-generics-demo/linked-list.ts
+++ 09-classes-generics-demo/linked-list.ts
@@ -1,6 +1,31 @@
-class ListNode {}
+class ListNode<T> {
+  next?: ListNode<T>;
 
-class LinkedList {
-  private root?: ListNode;
-  private length = 0;
+  constructor(public value: T) {}
 }
+
+class LinkedList<T> {
+  private root?: ListNode<T>;
+  private length = 0;
+
+  add = (value: T) => {
+    const node = new ListNode(value);
+
+    if (!this.root) {
+      this.root = node;
+    } else {
+      let current = this.root;
+
+      while (current.next) {
+        current = current.next;
+      }
+
+      current.next = node;
+    }
+
+    this.length++;
+  }
+}
+
+const numberList = new LinkedList<number>();
+const nameList = new LinkedList<string>();
