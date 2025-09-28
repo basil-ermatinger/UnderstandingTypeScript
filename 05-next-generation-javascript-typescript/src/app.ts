9caed9853e9d2023d64f9fab68cf305b5743dcc4
Basil Ermatinger <62306996+Vrijl@users.noreply.github.com>
2025-09-28 15:48:55 +0200

    Examples for typescript typeof

--- 05-next-generation-javascript-typescript/src/app.ts
+++ 05-next-generation-javascript-typescript/src/app.ts
@@ -1,4 +1,6 @@
-// Examples for const and let
+/******************************************************************
+ * const and let
+ ******************************************************************/
 
 const userName = "Basil";
 let age = 30;
@@ -11,7 +13,9 @@ function add(a: number, b: number) {
   return result;
 }
 
-// Examples of arrow functions
+/******************************************************************
+ * Arrow Functions
+ ******************************************************************/
 
 const addAsArrowFunc = (a: number, b: number) => {
   return a + b;
@@ -21,8 +25,6 @@ const addAsArrowFuncShort = (a: number, b: number) => a + b;
 
 const printOutput: (a: number | string) => void = output => console.log(output);
 
-console.log(addAsArrowFunc(2, 5));
-console.log(addAsArrowFuncShort(10, 10));
 printOutput(add(8, 8));
 
 const button = document.querySelector("button");
@@ -31,13 +33,15 @@ if(button) {
   button.addEventListener("click", event => console.log(event));
 }
 
-// Examples for default function parameters
+/******************************************************************
+ * Default Function Parameters
+ ******************************************************************/
 
 const addWithDefault = (a: number, b: number = 2) => a + b;
 
-console.log(addWithDefault(3));
-
-// Examples for spread operator
+/******************************************************************
+ * Spread Operator
+ ******************************************************************/
 
 const hobbies = ["Sports", "Cooking"];
 
@@ -45,10 +49,6 @@ const activeHobbies = ["Gaming", "Programming"];
 activeHobbies.push(...hobbies);
 
 const moreHobbies = ["Eating", ...hobbies, ...activeHobbies];
-
-console.log(hobbies);
-console.log(activeHobbies);
-console.log(moreHobbies);
 
 const person = {
   name: "Basil",
@@ -58,30 +58,25 @@ const person = {
 const copiedPerson = { ...person };
 copiedPerson.name = "Other Basil";
 
-console.log(person);
-console.log(copiedPerson);
-
-// Examples for rest parameters
+/******************************************************************
+ * Rest Parameters
+ ******************************************************************/
 
 const addWithRestPara = (...numbers: number[]) => numbers.reduce((curResult, curValue) => curResult + curValue, 0)
 
 const addFiveNumbers = addWithRestPara(1, 2, 3, 4, 5.5);
 const addTenNumbers = addWithRestPara(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-console.log(addFiveNumbers);
-console.log(addTenNumbers);
-
 const subtractWithRestParaThreeNumbers = (...numbers: [number, number, number]) => numbers.reduce((curResult, curValue) => curResult - curValue, numbers[0]);
 
 const subtractFiveNumbers = subtractWithRestParaThreeNumbers(10, 3, 2);
-console.log(subtractFiveNumbers);
 
-// Examples for array and object destruction
+/******************************************************************
+ * Array and Object Destruction
+ ******************************************************************/
 
 const games = ["Fallout", "Skyrim", "GTA", "Final Fantasy"]
 const [game1, game2, ...remainingGames] = games;
-
-console.log(`${game1}\n${game2}\n${remainingGames}`);
 
 const game = {
   gameName: game1,
@@ -90,6 +85,3 @@ const game = {
 }
 
 const { gameName, genre, durationToComplete: durationInHour } = game;
-
-console.log(`The ${genre} game ${gameName} needs ${durationInHour} hours to be completed.`);
-
