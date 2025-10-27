38c2e070d0c22721baa54608ef83b0d44218e0b7
Basil Ermatinger <62306996+Vrijl@users.noreply.github.com>
2025-10-27 19:43:47 +0100

    Examples for Readonly Types & Optional Mapping

--- 10-deriving-types-from-types/mapped-types.ts
+++ 10-deriving-types-from-types/mapped-types.ts
@@ -24,3 +24,30 @@ let mathResults: Results<Operations> = {
   add: mathOperations.add(1, 2),
   subtract: mathOperations.subtract(5, 2)
 }
+
+/******************************************************************
+ * Readonly Types & Optional Mapping
+ ******************************************************************/
+
+type OperationsOptional = {
+  multiply: (a: number, b: number) => number;
+  divide: (a: number, b: number) => number;
+}
+
+type ResultsOptional<T> = {
+  readonly [Key in keyof T]?: number
+}
+
+let mathOperationsOptional: OperationsOptional = {
+  multiply(a: number, b: number) {
+    return a * b
+  },
+  divide(a: number, b: number) {
+    return a / b;
+  }
+}
+
+let mathResultsOptional: ResultsOptional<OperationsOptional> = {
+  multiply: mathOperationsOptional.multiply(1, 2),
+  divide: mathOperationsOptional.divide(1, 2)
+}
